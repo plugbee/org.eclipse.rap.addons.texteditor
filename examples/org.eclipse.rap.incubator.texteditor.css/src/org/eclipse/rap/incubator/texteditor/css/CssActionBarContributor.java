4c0670cd43009ecdfae145692d81c9790b465cb0
Amine Lajmi <amine@plugbee.com>
2017-01-29 21:55:50 +0100

    css editor, initial import

---
+++ examples/org.eclipse.rap.incubator.texteditor.css/src/org/eclipse/rap/incubator/texteditor/css/CssActionBarContributor.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2016 PlugBee. All rights reserved.
+ * 
+ * This program and the accompanying materials are made available 
+ * under the terms of the Eclipse Public License v1.0 which 
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * </copyright>
+ */
+package org.eclipse.rap.incubator.texteditor.css;
+
+import org.eclipse.rap.incubator.texteditor.BasicTextEditorContributor;
+
+public class CssActionBarContributor extends BasicTextEditorContributor {
+
+	public CssActionBarContributor() {
+		// TODO Auto-generated constructor stub
+	}
+}
